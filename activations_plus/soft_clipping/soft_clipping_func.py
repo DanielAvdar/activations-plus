140bbe980010f0bcb4d7b89792d30500c801d615
DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>
2025-04-17 13:01:58 +0300

    Enhance SoftClipping documentation with detailed class and method descriptions
    Signed-off-by: DanielAvdar <66269169+DanielAvdar@users.noreply.github.com>

--- activations_plus/soft_clipping/soft_clipping_func.py
+++ activations_plus/soft_clipping/soft_clipping_func.py
@@ -8,20 +8,42 @@ class SoftClipping(torch.nn.Module):
     This activation function smoothly limits the range of activations, preventing extreme values
     without hard truncation. It is particularly useful for stabilizing neural network training.
 
-    Attributes:
-        min_val (float): The minimum value of the activation range.
-        max_val (float): The maximum value of the activation range.
-        clip_func (callable): The function used for clipping.
 
-    Methods:
-        forward(x): Computes the Soft Clipping activation for the input tensor `x`.
     """
 
     def __init__(self, x_min=-1.0, x_max=1.0, clip_func=torch.sigmoid):
+        """
+        SoftClipping is a configurable module for applying soft clipping to values within
+        a specified range. The module uses a differentiable non-linear clipping function
+        to constrain input values between a defined minimum and maximum range, facilitating
+        gradient-based optimization and control over numerical saturation.
+
+        The class allows the customization of the clipping function as well as the range
+        for clipping, which makes it applicable in a variety of machine learning tasks
+        requiring such transformations.
+
+        :param x_min: The minimum value to which the input will be clipped. Defaults to -1.0.
+        :type x_min: float
+        :param x_max: The maximum value to which the input will be clipped. Defaults to 1.0.
+        :type x_max: float
+        :param clip_func: A differentiable function used to softly clip the input values.
+                          This function determines the softness and shape of the transition
+                          between unclipped and clipped regions. Defaults to torch.sigmoid.
+        :type clip_func: Callable
+        """
         super(SoftClipping, self).__init__()
         self.min_val = x_min
         self.max_val = x_max
         self.clip_func = clip_func
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Applies a transformation to the input tensor by scaling and clipping its
+        values based on predefined `min_val` and `max_val`, using a clipping function.
+
+        :param x: Input tensor to be transformed.
+        :type x: torch.Tensor
+        :return: Transformed tensor after applying the scaling and clipping operation.
+        :rtype: torch.Tensor
+        """
         return self.min_val + (self.max_val - self.min_val) * self.clip_func(x)
